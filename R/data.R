dcdc709356273d75bb499a7de716abedcaf0a849
Loes Soetens <loes.soetens@rivm.nl>
2018-06-08 14:01:54 +0200

    fix bug

--- R/data.R
+++ R/data.R
@@ -1,7 +1,7 @@
 #' Example data for running the cluster app
 #'
 #' A dataset containing the idnumber, time of symptom onset, latitude and longitude and genetic sequence code for infectious disease cases
-#' @name example
+#' @name ClusterData
 #' \itemize{
 #'   \item id. id number of the case
 #'   \item time. time of symptom onset of the case (or any other time variable)
